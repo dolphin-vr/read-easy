f2b3f2d449e1547a754a396974d2cccff43b8ab1
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2023-08-02 01:15:35 +0300

    Added theme-switcher.js

---
+++ src/js/theme-switcher.js
@@ -0,0 +1,31 @@
+// The following settings must be set for the functionality to work:
+// for light theme
+// :root {
+//   --bg-color: #f1f2f6;
+//   --text-color: : #57606f;
+// }
+// for a dark theme
+// :root[theme='dark'] {
+//   --bg-color: #57606f;
+//   --text-color: : #f1f2f6;
+// }
+
+const initTheme = () => {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.setAttribute('theme', 'dark');
+  } else {
+    document.documentElement.removeAttribute('theme');
+  }
+};
+
+const switchTheme = () => {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.removeAttribute('theme');
+    localStorage.removeItem('theme');
+  } else {
+    document.documentElement.setAttribute('theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+};
+
+module.exports = { initTheme, switchTheme };
