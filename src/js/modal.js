23ce7b0f27ef6471114b6c7043ca73045369a7c9
OleksiiDmytruk <ljoshadmytruk@gmail.com>
2023-08-04 02:46:50 +0300

    modal open/close

---
+++ src/js/modal.js
@@ -0,0 +1,18 @@
+const refs = {
+  openModalBtnEl: document.querySelector('.js-modal-open'),
+  modalEl: document.querySelector('.js-modal-backdrop'),
+  closeModalBtnEL: document.querySelector('.js-modal-close-btn'),
+};
+
+refs.openModalBtnEl.addEventListener('click', openModal);
+refs.closeModalBtnEL.addEventListener('click', closeModal);
+
+function openModal() {
+  refs.modalEl.classList.toggle('is-hidden');
+  document.body.classList.toggle('no-scroll');
+}
+
+function closeModal() {
+  refs.modalEl.classList.toggle('is-hidden');
+  document.body.classList.toggle('no-scroll');
+}
