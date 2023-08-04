62315ef888cc1674fcec8c02655569a504b43215
OleksiiDmytruk <ljoshadmytruk@gmail.com>
2023-08-05 00:33:14 +0300

    modal add shop

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,19 @@
 const refs = {
+  bodyEl: document.querySelector('body'),
   openModalBtnEl: document.querySelector('.js-modal-open'),
   modalEl: document.querySelector('.js-modal-backdrop'),
   closeModalBtnEL: document.querySelector('.js-modal-close-btn'),
 };
-console.log(refs.openModalBtnEl);
+
 refs.openModalBtnEl.addEventListener('click', openModal);
 refs.closeModalBtnEL.addEventListener('click', closeModal);
 
 function openModal() {
   refs.modalEl.classList.toggle('is-hidden');
-  document.body.classList.toggle('no-scroll');
+  refs.bodyEl.classList.toggle('no-scroll');
 }
 
 function closeModal() {
   refs.modalEl.classList.toggle('is-hidden');
-  document.body.classList.toggle('no-scroll');
+  refs.bodyEl.classList.toggle('no-scroll');
 }
