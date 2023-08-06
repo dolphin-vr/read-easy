0110cf30fcd241bae13104fc5334d8a025987a5d
EvgeniyMoskovets <Caprizzon@gmail.com>
2023-08-06 18:08:03 +0300

    authorization v3
    Розкоментував частину коду, поправив трохи стилі

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import './js/modal';
 import './js/authorization-modal';
-// import './js/authorization';
+import './js/authorization';
 import './js/shoppinglist';
 
 import { homeMarkup } from './js/home';
 import { activePage } from './js/api';
-
 
 activePage();
 homeMarkup();
