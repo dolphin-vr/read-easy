902284b9abd9d55baa4a948b33d0fd8dcb47fe29
peogeka <peo.geka@gmail.com>
2023-08-06 14:02:33 +0200

    header

--- src/js/header-burger.js
+++ src/js/header-burger.js
@@ -1,21 +1,14 @@
-const menuBtn = document.querySelector('.menu-btn');
-const backdrop = document.querySelector('.backdrop[data-menu]');
-const imgMenu = menuBtn.querySelector('.img-menu');
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
 
-menuBtn.addEventListener('click', () => {
-  const isMenuOpen = menuBtn.getAttribute('data-menu-open') === 'true';
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  if (isMenuOpen) {
-    // Закрийте модальне вікно, якщо воно відкрите
-    backdrop.classList.add('is-hidden');
-    menuBtn.setAttribute('data-menu-open', 'false');
-    menuBtn.setAttribute('aria-expanded', 'false');
-    imgMenu.src = '../img/align-left (1).jpg'; // початкове зображення кнопки
-  } else {
-    // Відкрийте модальне вікно, якщо воно закрите
-    backdrop.classList.remove('is-hidden');
-    menuBtn.setAttribute('data-menu-open', 'true');
-    menuBtn.setAttribute('aria-expanded', 'true');
-    imgMenu.src = '../img/x-close (1).jpg'; //  нове зображення кнопки
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
   }
-});
+})();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './js/modal';
-// import './js/authorization-modal';
+import './js/authorization-modal';
 // import './js/authorization';
 import { homeMarkup } from './js/home';
 
