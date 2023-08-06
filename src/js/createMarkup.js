1df8f1cfb1a9cad956e9300f1878f65d8216a62f
vadivai <vaikanvn@gmail.com>
2023-08-07 01:40:35 +0300

    funds6.3

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -1,12 +1,8 @@
 export {createMarkupFunds }
 
-const createMarkupFunds = (array) => {
-  return array.map(({ title, url, img }, idx) =>
-    `<li>
+const createMarkupFunds = array => array.map(({ title, url, img }, idx) => `<li>
         <a class ="funds-href" href="${url}" target="_blank">
             <div class="funds-number">${(idx + 1).toString().padStart(2, 0)}</>
             <img class="funds-img" src="${img}" alt="${title}" height="32"/>
         </a>
-      </li>
-      `).join('')
-};
+      </li>`).join('');
