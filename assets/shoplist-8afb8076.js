1eb996ac8de18be3d0fca0eb7d22235ae1e8b778
dolphin-vr <124384373+dolphin-vr@users.noreply.github.com>
2023-08-07 23:09:11 +0000

    Deploying to gh-pages from @ dolphin-vr/read-easy@053f5c4a227d22f5c64285e2c133d5b124823746 🚀

--- assets/shoplist-8afb8076.js
+++ assets/shoplist-8afb8076.js
@@ -1,4 +1,4 @@
-import{s as o}from"./api-firebase-dcb211eb.js";const p="/read-easy/assets/sprite-c6a76667.svg";o("test@g.com","12345678").then(s=>{console.log("signInApp success"),console.log(s)}).catch(s=>{console.log("signInApp wrong")});const t=[{_id,title,author,list_name,description,book_image,buy_links:[url1,url2,url3]}];t.map(s=>n(s)).join("");function n(s){return`<li class="shopping-card">
+import{s as o}from"./api-firebase-2c29aaa6.js";const p="/read-easy/assets/sprite-c6a76667.svg";o("test@g.com","12345678").then(s=>{console.log("signInApp success"),console.log(s)}).catch(s=>{console.log("signInApp wrong")});const t=[{_id,title,author,list_name,description,book_image,buy_links:[url1,url2,url3]}];t.map(s=>n(s)).join("");function n(s){return`<li class="shopping-card">
       <button class="button-trash js-trash" aria-label="Close window" type="button"  id="${s._id}><svg class="icon"><use href="${p}#trash"></use></svg></button>
       <img src="${s.book_image}" alt="${s.titel}" class="shopping-book-img" />
          <div class="card-item"
