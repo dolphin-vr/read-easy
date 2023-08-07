c45aff09debd4e59a17540621922d4d66aac30fe
dolphin <fun53@ukr.net>
2023-08-07 21:43:45 +0300

    for-shop

--- src/js/shop1.js
+++ src/js/shop1.js
@@ -1,44 +1,27 @@
-const shoplist = [
-   {_id,
-   title,
-   author,
-   category,
-   descript,
-   cover,
-   shops: [url1, url2, "і так далі"]
-}
-]
+import { bookShopsMurkup } from "./shopslist";
+
+// const shoplist = [
+//    {_id,
+//    title,
+//    author,
+//    list_name,
+//    description,
+//    book_image,
+//    buy_links: [{"name": "Amazon", "url": "https://www.amazon..."}]
+// }
+// ]
+
 const listOfBooks = shoplist.map(el => createBookCard(el)).join('');
 
 function   createBookCard(book){
    return `<li class="item">
       <button class="trash js-trash" aria-label="Close window" type="button"  id="${book._id}><svg class="icon"><use href="${sprite}#trash"></use></svg></button>
-      <img src="${book.cover}" alt="">
+      <img src="${book.book_image}" alt="">
      <div class="card2">
      <h2 class="book-title">${book.title}</h2>
-     <h3 class="book-categ">${book.category}</h3>
-     <p class="book-descr">${book.descript}</p>
+     <h3 class="book-categ">${book.list_name}</h3>
+     <p class="book-descr">${book.description}</p>
      <p class="book-author">${book.author}</p>
-     </div>${bookShopsMurkup(book.shops)}
+     </div>${bookShopsMurkup(book.buy_links)}
    </li>`
-}
-
-function bookShopsMurkup(shops){
-   
-const iconPaths = [
-   {
-     name: "Amazon",
-     path: shop1png,
-   },
-   {
-     name: "Apple Books",
-     path: shop2png,
-   },
-   {
-     name: "Bookshop",
-     path: shop3png,
-   }
- ];
-
- return 'shops list'
 }
