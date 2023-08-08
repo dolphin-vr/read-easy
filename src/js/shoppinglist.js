ebeec857b5003e30abd4a690fac854ce868222f8
grebenik72 <grebenik72@gmail.com>
2023-08-08 11:37:33 +0300

    shop6

--- src/js/shoppinglist.js
+++ src/js/shoppinglist.js
@@ -25,54 +25,48 @@ const shoplist = [
    list_name,
    description,
    book_image,
-   buy_links: [url1, url2, url3]
+   buy_links,
    }
 ]
+
+ function bookShopsMurkup(shops) { 
+const buy_links = [
+   {
+      "name": "Amazon",
+      "url": "https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20"
+    },
+   {
+      "name": "Apple Books",
+      "url": "https://goto.applebooks.apple/9781250144058?at=10lIEQ"
+    },
+   {
+      "name": "Bookshop",
+      "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781250144058&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DWISH"
+    },
+ ];
+ return 'shops list'
+}
    
 const listOfBooks = shoplist.map(el => createBookCard(el)).join('');
 
 function   createBookCard(book){
-   return `<li class="shopping-card">
+   return `<li class="shop-card">
       <button class="button-trash js-trash" aria-label="Close window" type="button"  id="${book._id}><svg class="icon"><use href="${sprite}#trash"></use></svg></button>
-      <img src="${book.book_image}" alt="${book.titel}" class="shopping-book-img" />
+      <img src="${book.book_image}" alt="${book.title}" class="shop-book-img" />
          <div class="card-item"
-         <p class="shopping-book-name">${book.titel}</p>
-         <p class="shopping-book-category">${book.list_name}</p>
-         <div class="shopping-book-info">
+         <p class="shop-book-name">${book.title}</p>
+         <p class="shop-book-category">${book.list_name}</p>
+         <div class="shop-book-info">
          <p> ${book.description}</p>
          </div>
          <p class="book-author">${book.author}</p>
-         </div>${bookShopsMurkup(book.shops)}
+         </div>${bookShopsMurkup(book.buy_links)}
          </li>`
 }
- function bookShopsMurkup(shops) {
-   
-const iconPaths = [
-   {
-     name: "Amazon",
-     path: shop1png,
-   },
-   {
-     name: "Apple Books",
-     path: shop2png,
-   },
-   {
-     name: "Bookshop",
-     path: shop3png,
-   }
- ];
-
- return 'shops list'
-}
-// <!-- shopping-list page is empty -->
-const emptyPage = document.querySelector(`#page-empty`)
-function showShoppingListEmpty() {
-   console.log(emptyPage);
-}
-
+ 
 // remove book from shoppinglist
 
-const shoppingRemoveBtn = document.querySelector('.js-shopping-remove-btn');
+const shoppingRemoveBtn = document.querySelector('.js-trash');
 
 console.log(shoppingRemoveBtn);
     
