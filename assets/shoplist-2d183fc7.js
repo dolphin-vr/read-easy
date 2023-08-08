7268150e24cd5c7baa08ebf955469038e1bb8880
dolphin-vr <124384373+dolphin-vr@users.noreply.github.com>
2023-08-08 00:46:00 +0000

    Deploying to gh-pages from @ dolphin-vr/read-easy@466b64e083b0d4fa890c2a4b886a82a702fde50a 🚀

--- assets/shoplist-2d183fc7.js
+++ assets/shoplist-2d183fc7.js
@@ -1,4 +1,4 @@
-import{s as o,a as p}from"./sprite-596ea2eb.js";o("test@g.com","12345678").then(s=>{console.log("signInApp success"),console.log(s)}).catch(s=>{console.log("signInApp wrong")});const n=[{_id,title,author,list_name,description,book_image,buy_links:[url1,url2,url3]}];n.map(s=>t(s)).join("");function t(s){return`<li class="shopping-card">
+import{s as o,a as p}from"./sprite-71a96cf9.js";o("test@g.com","12345678").then(s=>{console.log("signInApp success"),console.log(s)}).catch(s=>{console.log("signInApp wrong")});const n=[{_id,title,author,list_name,description,book_image,buy_links:[url1,url2,url3]}];n.map(s=>t(s)).join("");function t(s){return`<li class="shopping-card">
       <button class="button-trash js-trash" aria-label="Close window" type="button"  id="${s._id}><svg class="icon"><use href="${p}#trash"></use></svg></button>
       <img src="${s.book_image}" alt="${s.titel}" class="shopping-book-img" />
          <div class="card-item"
