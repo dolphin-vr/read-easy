f98112798ce6b2b4b671a445c94aa5d8a30cc8ec
dolphin-vr <124384373+dolphin-vr@users.noreply.github.com>
2023-08-08 19:10:55 +0000

    Deploying to gh-pages from @ dolphin-vr/read-easy@4592c7115b44d2364bbd7e652443a0b884063e1f 🚀

--- assets/shoppinglist-d726d5d6.js
+++ assets/shoppinglist-d726d5d6.js
@@ -1,4 +1,4 @@
-import{a as h,b as r,c,d as g,g as m,i as u}from"./sprite-a5093366.js";const d=[{name:"Amazon",path:r},{name:"Apple Books",path:c},{name:"Bookshop",path:g}];function y(s){return`<li class="shopping-card">
+import{a as h,b as r,c,d as g,g as m,i as u}from"./sprite-105cc8f8.js";const d=[{name:"Amazon",path:r},{name:"Apple Books",path:c},{name:"Bookshop",path:g}];function y(s){return`<li class="shopping-card">
      <button class="button-trash js-trash" aria-label="Close window" type="button"  id="${s._id}>
      <svg class="trash-icon"><use href="${h}#trash"></use></svg></button>
      <img src="${s.book_image}" alt="${s.title}" class="shopping-book-img" />
