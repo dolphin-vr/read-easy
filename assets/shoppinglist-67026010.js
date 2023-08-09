226455d61680de06517bd9e99ae0f799ae0cccc8
dolphin-vr <124384373+dolphin-vr@users.noreply.github.com>
2023-08-09 23:00:56 +0000

    Deploying to gh-pages from @ dolphin-vr/read-easy@de0537684fc3444349377cc040a07eef322b6ddd 🚀

--- assets/shoppinglist-67026010.js
+++ assets/shoppinglist-67026010.js
@@ -1,5 +1,5 @@
-import{a as h,b as r,c,d as g,g as m,i as u}from"./log-out-00fdaf98.js";const d=[{name:"Amazon",path:r},{name:"Apple Books",path:c},{name:"Bookshop",path:g}];function y(s){return`<li class="shopping-card">
-<button class="button-trash js-trash" aria-label="Close window" type="button"  id="${s._id}>
+import{a as h,b as r,c,d as g,g as m,i as u}from"./log-out-57ed5443.js";const d=[{name:"Amazon",path:r},{name:"Apple Books",path:c},{name:"Bookshop",path:g}];function y(s){return`<li class="shopping-card">
+<button class="button-trash js-trash" aria-label="Close window" type="button"  id="${s._id}">
 <svg class="trash-icon">
 <use href="${h}#trash"></use>
 </svg>
