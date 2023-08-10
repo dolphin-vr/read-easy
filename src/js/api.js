f0618a20e5f2eb7076962c2d7f0817381c5de009
dolphin <fun53@ukr.net>
2023-08-10 13:07:57 +0300

    sp2

--- src/js/api.js
+++ src/js/api.js
@@ -12,21 +12,38 @@ const optNotiflx = {
   fontSize: '28px',
 };
 
-function showActivePage() {
+const refs = {
+  catlisSec: document.querySelector('.js-catlist'),
+  fundsSec: document.querySelector('.js-funds'),
+  mainSec: document.querySelector('.js-main'),
+  btnHome: document.querySelector('.btnHome'),
+  btnSL: document.querySelector('.btnSL'),
+}
 
+refs.btnHome.addEventListener('click', showHomePage);
+refs.btnSL.addEventListener('click', showShoppingList);
+
+function showActivePage() {
+ console.dir('catlist= ', refs.catlisSec)
   let currentPage = JSON.parse(sessionStorage.getItem(keyPage));
   if (currentPage === 'shoppinglist') {
+    console.log('if showShoppingList')
     showShoppingList();
   } else {
+    console.log('if showHomePage')
     showHomePage();
   }
 }
 
 function showShoppingList() {
+  console.log('func showShoppingList')
+  refs.catlisSec.classList.replace('category', 'hidden');
   sessionStorage.setItem(keyPage, JSON.stringify('shoppinglist'));
 }
 
 function showHomePage() {
+  console.log('func showHomePage')
+  refs.catlisSec.classList.replace('hidden', 'category');
   sessionStorage.setItem(keyPage, JSON.stringify('homepage'));
   homeMarkup();
 }
