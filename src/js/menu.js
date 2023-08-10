894e3f9894f969ef82c73250b93d3d4ee9ebccf0
dolphin <fun53@ukr.net>
2023-08-11 02:18:30 +0300

    p2

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,10 @@
 function burgerMenu() {
   const mobileMenu = document.querySelector('[data-menu]');
   const openMenuBtn = document.querySelector('[data-menu-open]');
-  const closeMenuBtn = document.querySelector('[data-menu-close]');
+  // const closeMenuBtn = document.querySelector('[data-menu-close]');
 
   const toggleMenu = () => {
-    const isMenuOpen =
+    // const isMenuOpen =
     // openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     // openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
