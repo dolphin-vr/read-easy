0556833e3cc7f7338730ce2df89e58fe037cf0e4
dolphin-vr <124384373+dolphin-vr@users.noreply.github.com>
2023-08-11 15:22:03 +0000

    Deploying to gh-pages from @ dolphin-vr/read-easy@8a0d5c988741cbc9436eb199caae83b3d18bc659 🚀

--- assets/shoppinglist-81a05777.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./main-449aa79c.js";const e=document.querySelector(".js-trash");e.addEventListener("click",a);function a(t){const s=t.target.closest(".js-trash");if(t.target.closest(".js-trash")){const o=s.id;r(o)}}
